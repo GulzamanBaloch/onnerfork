259b3b2e6d5f9521d140dfcaafc06c11b8ea38c4
gulzamankhan <gulzamman555@gmail.com>
2026-04-10 21:37:19 -0700

    second commit gul

--- obj/Debug/net8.0/WebApplication_test.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication_test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8fd4deffdb93fad3ac3d35bea6b4d0ddc395fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336112dc67c9b77686a7f15694d5e26c89989485")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
